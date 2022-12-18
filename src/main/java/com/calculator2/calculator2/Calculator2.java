20112d1863851aeded6f3f509249074a9e69e651
Tom <tomwinzu@gmail.com>
2022-12-18 16:49:35 +1100

    Initial commit

---
+++ src/main/java/com/calculator2/calculator2/Calculator2.java
@@ -0,0 +1,14 @@
+package com.calculator2.calculator2;
+
+public class Calculator2 {
+    private final String content;
+
+           public Calculator2(String content){
+
+    this.content=content;}
+
+    public String getContent() {
+
+        return content;
+    }
+}
